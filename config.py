8c2675794f82471b5be1a56e70557c9eec510cbe
caiseyann <caiseyann4@gmail.com>
2020-09-25 20:43:46 +0300

    added function test

--- config.py
+++ config.py
@@ -3,9 +3,16 @@ from flask_script import Manager, Server
 from app.models import User, Blog, Comment, PhotoProfile
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server', Server)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
