9986d8c61f4db7d219ee44406e0e561c4ed8f7a1
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:20:34 +0300

    class user created

--- app/models.py
+++ app/models.py
@@ -31,3 +31,17 @@ class User(UserMixin, db.Model):
     profile_pic_path = db.Column(db.String(255))
     blogs = db.relationship("Blog", backref= "user", lazy="dynamic")
     comments = db.relationship("Comment", backref= "user", lazy="dynamic")
+
+     @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
