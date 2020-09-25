6a08160343daaf4b4860d707d84a13fce8d66180
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:20:03 +0300

    class under construction

--- app/models.py
+++ app/models.py
@@ -19,3 +19,15 @@ class Quote:
         self.author = author
         self.quote = quote
         self.permalink = permalink
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String(255))
+    blogs = db.relationship("Blog", backref= "user", lazy="dynamic")
+    comments = db.relationship("Comment", backref= "user", lazy="dynamic")
