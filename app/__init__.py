f966223f5dd2156fe4ff8f75d816107b30b61b91
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:12:19 +0300

    defining functions in init

--- app/__init__.py
+++ app/__init__.py
@@ -19,3 +19,34 @@ photos = UploadSet('photos', IMAGES)
 
 mail = Mail()
 simple = SimpleMDE()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+   
+
+    # Intializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+
+    # Will add the views and forms
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
+
+    return app
