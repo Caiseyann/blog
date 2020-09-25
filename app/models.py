2200efcb40ca91fb38deb7419d22825f023e5942
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:26:55 +0300

    class master created

--- app/models.py
+++ app/models.py
@@ -97,3 +97,11 @@ class Comment(db.Model):
         comment = Comment.query.filter_by(id = c_id).first()
         db.session.delete(comment)
         db.session.commit()
+
+
+class Member(db.Model):
+    __tablename__ = 'members'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(255))
+    username = db.Column(db.String(255), index = True)
