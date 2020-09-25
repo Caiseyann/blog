8aba80ed8929244570fb61a207c45a5c72bbf771
caiseyann <caiseyann4@gmail.com>
2020-09-25 20:48:46 +0300

    class ProdConfig(Config)

--- config.py
+++ config.py
@@ -26,3 +26,13 @@ class Config:
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
