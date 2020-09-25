f7eb2b25ea07eca2ee5ad989df8514eb3f11f85b
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:34:52 +0300

    defining functions

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -18,3 +18,12 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Password',validators = [Required()])
     subscribe = BooleanField('Subscribe to News letters')
     submit = SubmitField('Sign Up')
+
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
