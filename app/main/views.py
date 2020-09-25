9f584fd80ee3018e365ae455c30d618424bc6275
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:46:38 +0300

    defining function index

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,25 @@ from ..request import get_quote
 from flask_login import login_required, current_user
 import markdown2
 from ..email import mail_message
+
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'GraphersBlog'
+    quote = get_quote()
+    comments = Comment.query.all()
+    allBlogs = Blog.query.all()
+    del_blog = request.args.get('delete_this')
+    del_comment = request.args.get('delete_comment')
+    if del_blog:
+        Blog.delete_blog(b_id=del_blog)
+        return redirect(url_for('main.index'))
+    elif del_comment:
+        Comment.delete_comment(c_id=del_comment)
+        return redirect(url_for('main.index'))
+    else:
+        return render_template('index.html', title = title, blogs = allBlogs, quote = quote, comments = comments)
