576e5fc9d5d6a76537b3cd1149041dd81b23c592
caiseyann <caiseyann4@gmail.com>
2020-09-25 20:57:23 +0300

    defining models

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,16 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from config import config_options
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+photos = UploadSet('photos', IMAGES)
+
+mail = Mail()
+simple = SimpleMDE()
