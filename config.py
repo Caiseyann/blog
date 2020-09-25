201f0ad5acdef3200342c88119733b99155f7b13
caiseyann <caiseyann4@gmail.com>
2020-09-25 20:47:19 +0300

    created class config

--- config.py
+++ config.py
@@ -1,3 +1,28 @@
 import os
 
 
+class Config:
+    '''
+    General configuration parent class
+    '''                                          
+                                                                                                  
+
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+   
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'BLOG'
+    SENDER_EMAIL = os.environ.get('MAIL_USERNAME')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    # simplemde confirgurations
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
