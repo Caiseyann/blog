b19c131ca2147b39310921ccbd3e07723b0a1829
caiseyann <caiseyann4@gmail.com>
2020-09-25 20:44:48 +0300

    defined function make_shell

--- config.py
+++ config.py
@@ -16,3 +16,12 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Blog = Blog, Comment = Comment)
+
+
+if __name__ == "__main__":
+    manager.run()
