ec67c722b78b3ed790e7cf37bcb0ad4deeffc3e0
caiseyann <caiseyann4@gmail.com>
2020-09-25 21:18:44 +0300

    defining functions

--- app/models.py
+++ app/models.py
@@ -3,3 +3,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
