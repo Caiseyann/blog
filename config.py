6a1501fdd92104e92dc27db3cf8bd10e14b1c258
caiseyann <caiseyann4@gmail.com>
2020-09-26 12:30:08 +0300

    installed gunicorn

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/blog'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/blog'
 
     # email configurations
     MAIL_SERVER = 'smtp.gmail.com'
@@ -33,7 +33,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:password@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestConfig(Config):
